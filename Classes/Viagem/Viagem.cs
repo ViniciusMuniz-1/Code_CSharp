677d76c6980225abed6e947839fb6614c7299969
José Vinicius Targino Muniz <20222014040010@ifrn.local>
2023-04-17 15:55:53 -0300

    Adição da classe viagem

--- Classes/Viagem/Viagem.cs
+++ Classes/Viagem/Viagem.cs
@@ -5,11 +5,28 @@ namespace Viagem
     class Viagem
     {
         public int distancia;
-        public int tempo;
+        public string[] tempo;
 
-        static void Main(string[] args)
+        public static double calc_vmedia(int distancia, string[] tempo){
+            int horas = int.Parse(tempo[0]);
+            int min = int.Parse(tempo[1]);
+
+            int tempot = horas + (min/60);
+
+            double vmedia = distancia/tempot;
+            return vmedia;
+        }
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Viagem x;
+            x = new Viagem();
+            Console.WriteLine("Digite a distância da viagem:");
+            x.distancia = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o tempo da viagem:");
+            x.tempo = Console.ReadLine().Split(":");
+            
+            Console.Write($"Velocidade média: {calc_vmedia(x.distancia, x.tempo)}km/h");
         }
     }
 }
