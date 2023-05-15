8687cb0b43efee915bc68114530231205b4dade7
José Vinicius Targino Muniz <20222014040010@ifrn.local>
2023-05-15 15:51:21 -0300

    Adição da questão Frete2 e Agenda(incompleta

---
+++ Classes/Agenda/Agenda.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Agenda
+{
+    class Agenda
+    {
+        private Compromisso[] comps;
+        private int k;
+        
+        public int Qtd{
+            get{
+                
+            }
+        }
+
+        public void Inserir(Compromisso c){
+            comps[k] = c;
+            k++;
+        }
+
+        public void Excluir(Compromisso c){
+            for(int i = 0; i < comps.Length; i++){
+                if(comps[i] == c){
+                    comps[i] = null;
+                    k--;
+                }
+            }
+        }
+
+        public Compromisso[] Listar(){
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
