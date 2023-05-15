aaa3e6d3ef6c5190594ccdbb856e3b2bae1d3336
José Vinicius Targino Muniz <20222014040010@ifrn.local>
2023-05-15 15:50:24 -0300

    Adição da questão Frete2 e Agenda(incompleta

---
+++ Classes/Frete2/Frete2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Frete2
+{
+    class Frete2
+    {
+        private double distancia;
+        private double peso;
+        public double Distancia{
+            get{
+                return this.distancia;
+            }
+            set{
+                if(value >= 0){
+                    this.distancia = value;
+                }
+            }
+        }
+
+        public double Peso{
+            get{
+                return this.peso;
+            }
+            set{
+                if(value > 0){
+                    this.peso = value;
+                }
+            }
+        }
+
+        public double ValorFrete{
+            get{
+                return 0.01 * this.peso * this.distancia;
+            }
+        }
+
+        public override string ToString(){
+            return $"Peso: {Peso}; Distância: {Distancia}; Valor Frete: {ValorFrete}";
+        }
+
+        static void Main(string[] args)
+        {
+            Frete2 a = new Frete2 { Peso = 20, Distancia = 10};
+            Console.WriteLine(a.ValorFrete);
+            Console.WriteLine(a);
+        }
+    }
+}
