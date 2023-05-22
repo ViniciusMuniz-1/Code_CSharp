84b24d65c08aa57687c0c3fb53bde657eef477c5
José Vinicius Targino Muniz <20222014040010@ifrn.local>
2023-05-22 15:54:10 -0300

    Adição da classe Media e a Q1 da avaliação 2 de 2022

---
+++ Avaliacao2022-2/Questao1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Questao1
+{
+    class Program
+    {
+        public static void Main(){
+            Console.WriteLine("Digite o país da equipe:");
+            string pais = Console.ReadLine();
+            Equipe equipe = new Equipe(pais);
+
+            int op = Menu();
+            while (op != 0)
+            {
+                switch (op)
+                {
+                  case 1: Inserir(); break;
+                  case 2: Excluir(); break;
+                  case 3: Listar(); break;
+                  case 4: Artilheiros(); break;
+                  case 5: Camisas(); break;
+                }               
+               op = Menu();    
+            }
+        }
+
+        public static int Menu(){
+            Console.WriteLine("0-Fim, 1-Inserir, 2-Excluir, 3-Listar, 4-Artilheiros, 5-Camisas: ");
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static void Inserir(){
+            Console.WriteLine("Inserir novo jogador");
+            Console.WriteLine("Informe o nome do jogador:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Informe o número da camisa dele:");
+            int camisa = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o número de gols desse jogador:");
+            int gols = int.Parse(Console.ReadLine);
+
+            Jogador jogador = new Jogador(nome, camisa, gols);
+
+            Console.WriteLine("Jogador inserido com sucesso");
+        }
+
+        public static void Listar(){
+            foreach (Jogador j in equipe.Listar())
+            {
+                Console.WriteLine(j);
+            }
+        }
+    }
+
+    class Equipe{
+        private string pais;
+        private Jogador[] jogs;
+
+        public void Inserir(Jogador j){
+            
+        }
+
+        public Jogador[] Listar(){
+
+        }
+
+        public Jogador[] Artilheiros(){
+
+        }
+
+        public Jogador[] Camisas(){
+
+        }
+
+        public void ToString(){
+
+        }
+    }
+    
+    class Jogador{
+        private string nome;
+        private int camisa;
+        private int numGols;
+
+        public Jogador(string nome, int camisa, int numGols){
+            this.nome = nome;
+            this.camisa = camisa;
+            this.numGols = numGols;
+        }
+
+        public void ToString(){
+            Console.WriteLine($"Nome: {nome} - Camisa: {camisa} - Número de gols: {numGols}");
+        }
+    }
+}
---
+++ Classes/Relacionamentos_Interfaces/MediaDisciplinas/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace MediaDisciplinas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDisciplina a = new DisciplinaAnual("Matemática", 80, 67, 70, 59, 60);
+            IDisciplina b = new DisciplinaAnual("Portuguese", 50, 67, 40, 59, 60);
+            IDisciplina c = new DisciplinaSemestral("Sociologia", 80, 67, 30);
+            IDisciplina d = new DisciplinaSemestral("Arte", 90, 100, 30);
+
+            Console.WriteLine(a.GetNome());
+            Console.WriteLine(a.CalcMediaParcial());
+            Console.WriteLine(a.CalcMediaFinal());
+            Console.WriteLine(b.GetNome());
+            Console.WriteLine(b.CalcMediaParcial());
+            Console.WriteLine(b.CalcMediaFinal());
+            Console.WriteLine(c.GetNome());
+            Console.WriteLine(c.CalcMediaParcial());
+            Console.WriteLine(c.CalcMediaFinal());
+            Console.WriteLine(d.GetNome());
+            Console.WriteLine(d.CalcMediaParcial());
+            Console.WriteLine(d.CalcMediaFinal());
+        }
+    }
+    interface IDisciplina{
+        string GetNome();
+        int CalcMediaParcial();
+        int CalcMediaFinal();
+    }
+
+    class DisciplinaAnual : IDisciplina{
+        private string nome;
+        private int nota1;
+        private int nota2;
+        private int nota3;
+        private int nota4;
+        private int notaFinal;
+
+        public DisciplinaAnual(string nome, int nota1, int nota2, int nota3, int nota4, int notaFinal){
+            this.nome = nome;
+            this.nota1 = nota1;
+            this.nota2 = nota2;
+            this.nota3 = nota3;
+            this.nota4 = nota4;
+            this.notaFinal = notaFinal;
+        }
+
+        public string GetNome(){
+            return this.nome;
+        }
+
+        public int CalcMediaParcial(){
+            return ((nota1*2)+(nota2*2)+(nota3*3)+(nota4*3))/10;
+        }
+
+        public int CalcMediaFinal(){
+            return (this.CalcMediaParcial()+notaFinal)/2;
+        }
+    }
+
+    class DisciplinaSemestral : IDisciplina{
+        private string nome;
+        private int nota1;
+        private int nota2;
+        private int notaFinal;
+
+        public DisciplinaSemestral(string nome, int nota1, int nota2, int notaFinal){
+            this.nome = nome;
+            this.nota1 = nota1;
+            this.nota2 = nota2;
+            this.notaFinal = notaFinal;
+        }
+
+        public string GetNome(){
+            return this.nome;
+        }
+
+        public int CalcMediaParcial(){
+            return ((nota1*2)+(nota2*3))/5;
+        }
+
+        public int CalcMediaFinal(){
+            return (this.CalcMediaParcial()+notaFinal)/2;
+        }
+    }
+}
