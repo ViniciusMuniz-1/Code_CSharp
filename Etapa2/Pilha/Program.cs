91bff59ec991a7d0fdfb57903f0fc4b8be394579
José Vinicius Targino Muniz <20222014040010@ifrn.local>
2023-05-31 14:28:44 -0300

    Adição da classe genérica Pilha

---
+++ Etapa2/Pilha/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Pilha
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Pilha<string> x = new Pilha<string>();
+            x.Push("C#");
+            x.Push("C++");
+            x.Push("Java");
+            Console.WriteLine(x.Peek());
+            while(x.Count > 0){
+                Console.WriteLine(x.Pop());
+            }
+        }
+    }
+
+    class Pilha<T>{
+        private T[] objs = new T[10];
+        private int cont=0;
+        
+        public int Count{
+            get { return cont; }
+        }
+
+        public int Clear(){
+            cont = 0;
+        }
+
+        public T Peek(){
+            if(cont == 0){throw new InvalidOperationException();}
+            return objs[cont];
+        }
+
+        public void Push(T obj){
+            if(cont == objs.Length){
+                Array.Resize(ref objs, 2 * objs.Length);
+            }
+            objs[cont] = obj;
+            cont++;
+        }
+
+        public T Pop(){
+            if(cont == 0){
+                throw new InvalidOperationException();
+            }
+            cont--;
+            return objs[cont];
+        }        
+    }
+}
